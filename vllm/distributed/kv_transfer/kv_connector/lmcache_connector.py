940213dd68cdbed70cfdb5895a3f2ae1e646dcd5
hongrui <1176380032@qq.com>
2025-07-07 03:55:57 +0000

    fix lmcache name and init

--- vllm/distributed/kv_transfer/kv_connector/lmcache_connector.py
+++ vllm/distributed/kv_transfer/kv_connector/lmcache_connector.py
@@ -34,7 +34,7 @@ class LMCacheConnector(KVConnectorBase):
         self.transfer_config = config.kv_transfer_config
         self.vllm_config = config
 
-        from lmcache.experimental.cache_engine import LMCacheEngineBuilder
+        from lmcache.v1.cache_engine import LMCacheEngineBuilder
         from lmcache.integration.vllm.utils import ENGINE_NAME
         from lmcache.integration.vllm.vllm_adapter import (
             RetrieveStatus, StoreStatus, init_lmcache_engine,
@@ -46,7 +46,8 @@ class LMCacheConnector(KVConnectorBase):
         # TODO (Jiayi): Find model_config, parallel_config, and cache_config
         self.engine = init_lmcache_engine(config.model_config,
                                           config.parallel_config,
-                                          config.cache_config)
+                                          config.cache_config,
+                                          config.scheduler_config)
         self.lmcache_engine_name = ENGINE_NAME
         self.lmcache_engine_builder = LMCacheEngineBuilder
 
